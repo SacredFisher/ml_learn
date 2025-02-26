520cf07029b668ed0b43c6d5af3992b6020d3a7f
Srikar Pamidi <srikar.pamidi@gmail.com>
2025-02-26 18:16:23 -0500

    added power, relu, basic arithmetic functions to engine

--- proj/karpathy-ml/micrograd/micrograd/engine.py
+++ proj/karpathy-ml/micrograd/micrograd/engine.py
@@ -59,6 +59,50 @@ class Value:
 
         return out 
 
+    def __pow__(self, other): 
+        assert isinstance(other, (int, float))
+        out =  Value(self.data**(other), (self, other), '^') 
+
+        def _backward(): 
+            out.grad = ((other) * (self.data ** (other - 1))) * out.grad
+
+        out._backward = _backward
+
+        return out 
+
+    def relu(self): 
+        out  = Value(0 if self.data < 0 else self.data, (self, ), 'relu()')
+
+        def _backward(): 
+            out.grad = (self.data > 1) * out.grad #in the case where self.data > 1, grad = 1, else grad = 0
+
+        out._backward = _backward
+
+        return out
+
+    def __radd__(self, other): #other + self
+        return self + other 
+
+    def __rmul__(self, other): #other * self 
+        return self * other
+
+    def __neg__(self): 
+        return self * -1
+    
+    def __sub__(self, other): 
+        return self + (-1 * other)
+
+    def __rsub__(self, other): 
+        return other + (-self)
+
+    def __truediv__(self, other): 
+        return self * other ** -1 
+
+    def __rtruediv__(self, other): 
+        return other * self ** -1
+
+    
+
 if __name__ == '__main__': 
     a = Value(1, _label ='a')
     b = Value(2, _label ='b')
